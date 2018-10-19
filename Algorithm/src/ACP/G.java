b1a3a246de2fabdec6b1cd1408f479037744bbb9
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-10-20 03:06:01 +0900

    10/20

--- Algorithm/src/ACP/G.java
+++ Algorithm/src/ACP/G.java
@@ -18,14 +18,14 @@ public class G {
 		String input = br.readLine();
 		
 		n = Integer.parseInt(input.split(" ")[0]);
-		k = Integer.parseInt(input.split(" ")[1])-1;
+		k = Integer.parseInt(input.split(" ")[1])-1; // 게이트 숫자-1 -> 부분 배열에서 역순(ar[n]>ar[n+1]이 나올 수 있는 횟수
 		
 		int[] ar = new int[n];
 		
 		input = br.readLine();
 		
 		for(int i=0 ; i<n ; i++){
-			ar[i] = Integer.parseInt(input.split(" ")[i])-1; //index와 맞춰주기 위함
+			ar[i] = Integer.parseInt(input.split(" ")[i])-1; // 승객의 순서 번호를 index와 맞춰주기 위함
 		}
 		
 		int max = -1;
