d0bb9e8c17ba96eba26c381b39175fba623f5a6b
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-11-22 15:01:19 +0900

    11/22

---
+++ Algorithm/src/DynamicProgramming/Padovan.java
@@ -0,0 +1,51 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Padovan {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+
+		ArrayList<Long> list = new ArrayList<>();
+		ListIterator<Long> it = list.listIterator();
+		list.add((long) 1);
+		list.add((long) 1);
+		list.add((long) 1);
+		list.add((long) 2);
+		list.add((long) 2);
+		list.add((long) 3);
+		list.add((long) 4);
+		list.add((long) 5);
+		list.add((long) 7);
+		list.add((long) 9);
+
+		for(int i=0 ; i<n ; i++){
+			int input = Integer.parseInt(br.readLine());
+			if(input<=list.size()){
+				sb.append(list.get(input-1)+"\n");
+			}
+			else{
+				while(input>list.size()){
+					list.add(list.get(list.size()-3)+list.get(list.size()-2));
+				}
+				sb.append(list.get(input-1)+"\n");
+			}
+		}
+		sb.deleteCharAt(sb.length()-1);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
