77e373de21c7165d537d1f19f9ae277eccc97255
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-02-09 18:09:36 +0900

    2/9

--- Algorithm/src/DivideAndConquer/Hanoi.java
+++ Algorithm/src/DivideAndConquer/Hanoi.java
@@ -1,47 +1,49 @@
 package DivideAndConquer;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Hanoi {
 
-	static void rec(int[] hanoi){
-
-			
-
-
-
-		/*if(1,0,나머지 || 0,1,나머지){
-			//리턴
-		}
-		else{
-			rec(hanoi);
-		}*/
-	}
-
-
+	static StringBuilder sb;
 
 	public static void main(String[] args) {
-		int[] hanoi = new int[3];
+
 		Scanner sc = new Scanner(System.in);
+
 		int n = sc.nextInt();
+
 		sc.close();
-		if(n>20 || n<1){
-			System.out.println("Wrong input");
+
+		if(n<=20){
+			sb = new StringBuilder();
+
+			hanoi(n, 1, 2, 3);
+
+			sb.deleteCharAt(sb.length()-1);
+
+			System.out.println((int)Math.pow(2, n)-1);
+			System.out.println(sb.toString());
 		}
+
 		else{
-			int[][] ar = new int[(int) Math.pow(2, n)-1][2];
-
-
-
-
-
-			rec(hanoi);
-			System.out.println(ar.length);
-			for(int i=0 ; i<ar.length ; i++){
-				System.out.print(ar[i][0]);System.out.print(" ");System.out.print(ar[i][1]);
-				System.out.println();
+			BigInteger result=new BigInteger("1");
+			for(int i=0 ; i<n ; i++) {
+				result=result.multiply(new BigInteger("2"));
 			}
+			result=result.subtract(new BigInteger("1"));
+			System.out.println(result);
 		}
 	}
 
+	private static void hanoi(int extras, int t1, int t2, int t3) {
+		if(extras==1){
+			sb.append(t1 + " " + t3 + "\n");
+		}
+		else{	
+			hanoi(extras-1, t1, t3, t2);
+			sb.append(t1 + " " + t3 + "\n");
+			hanoi(extras-1,t2,t1,t3);
+		}		
+	}	
 }
