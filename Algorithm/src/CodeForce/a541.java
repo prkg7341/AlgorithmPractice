3753023b347c91676fab7dd13ce2ac6d2a60e97b
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-02-23 21:37:58 +0900

    2/23

---
+++ Algorithm/src/CodeForce/a541.java
@@ -0,0 +1,31 @@
+package CodeForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a541 {
+	static int h0;
+	static int w0;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] input = br.readLine().split(" ");
+
+		int w1,h1,w2,h2;
+
+		w1 = Integer.parseInt(input[0]);
+		h1 = Integer.parseInt(input[1]);
+		w2 = Integer.parseInt(input[2]);
+		h2 = Integer.parseInt(input[3]);
+
+		h0 = h1+h2+2;
+		w0 = w1+2;
+		
+		int result = h0*w0-h1*w1-h2*w2-(w1-w2)*h2;
+
+		System.out.println(result);
+	}
+}
