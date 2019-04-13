8c7c2a1996fca391010a96cf2e544ed8280b54b5
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-04-13 09:53:29 +0900

    4/12

--- Algorithm/src/DivideAndConquer/DividePaper.java
+++ Algorithm/src/DivideAndConquer/DividePaper.java
@@ -1,101 +1,64 @@
 package DivideAndConquer;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DividePaper {
 
-	public DividePaper(int[][] paper, int x1, int y1, int x2, int y2, int[] count, int n){
-
-		devide(paper, x1, y1, x2, y2, count);
-		getAnswer(count);
-	}	
-
-
-	void devide(int[][] paper, int x1, int y1, int x2, int y2, int[] count) { //System.out.println("메쏘드 시작");
-		//int q=1; System.out.println(q + "번째 시도"); q++;
-		if(x1==x2){
-
-			//System.out.println("하나짜리 세는중");
-
-			if(paper[x1][y1]==-1){
-				//System.out.println("-1 하나짜리");
-				count[0]++;
-			}
-			else if(paper[x1][y1]==0){
-				//System.out.println("0 하나짜리");
-				count[1]++;
-			}
-			else if(paper[x1][y1]==1){
-				//System.out.println("1 하나짜리");
-				count[2]++;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] paper = new int[n][n];
+		int start = 0;
+		int end = n-1;
+		int[] array = new int[3];
+		for(int i=0 ; i<n ; i++){
+			String[] input = br.readLine().split(" ");
+			for(int j=0 ; j<n ; j++){
+				paper[i][j] = Integer.parseInt(input[j]);
 			}
 		}
+		devide(paper, start, start, end, end, array);
+		print(array);
+	}
 
-		else{//System.out.println("여러개짜리 세는중");
-			int check = (x2-x1)*(x2-x1);
-			for(int i=x1 ; i<x2 ; i++){
-				for(int j=y1 ; j<y2 ; j++){				
-					if(paper[i][j]==paper[i+1][j] && paper[i][j]==paper[i+1][j+1] && paper[i][j]==paper[i][j+1]){
-						check--;//모두 같으면 감소시킨다.
+	static void devide(int[][] paper, int startX, int startY, int endX, int endY, int[] array) {
+		int now = paper[startX][startY];
+		boolean same = true;
+
+		ar:
+			for(int i=startX ; i<=endX ; i++){
+				for(int j=startY ; j<=endY ; j++){				
+					if(paper[i][j]!=now){
+						same = false;
+						break ar;
 					}
 				}
 			}
-			//System.out.println("check is " +check);
 
-			if(check==0){
-				if(paper[x1][y1]==-1){
-					//System.out.println("-1 여러개짜리");
-					count[0]++;
-				}
-				else if(paper[x1][y1]==0){
-					//System.out.println("0 여러개짜리");
-					count[1]++;
-				}
-				else if(paper[x1][y1]==1){
-					//System.out.println("1 여러개짜리");
-					count[2]++;
-				}
-			}
+		if(same) array[now+1]++;
 
-			else{//int i=0; System.out.println("나눠"+ ++i);
-				devide(paper, x1, 					y1, 					(2*(x1+1)+(x2+1))/3-1, 	(2*(y1+1)+(y2+1))/3-1, 	count);
-				devide(paper, x1, 					(2*(y1+1)+(y2+1))/3, 	(2*(x1+1)+(x2+1))/3-1, 	((y1+1)+2*(y2+1))/3-1, 	count);
-				devide(paper, x1, 					((y1+1)+2*(y2+1))/3,	(2*(x1+1)+(x2+1))/3-1, 	y2, 					count);
-				devide(paper, (2*(x1+1)+(x2+1))/3, 	y1, 					((x1+1)+2*(x2+1))/3-1, 	(2*(y1+1)+(y2+1))/3-1, 	count);
-				devide(paper, (2*(x1+1)+(x2+1))/3, 	(2*(y1+1)+(y2+1))/3, 	((x1+1)+2*(x2+1))/3-1, 	((y1+1)+2*(y2+1))/3-1, 	count);
-				devide(paper, (2*(x1+1)+(x2+1))/3, 	((y1+1)+2*(y2+1))/3,	((x1+1)+2*(x2+1))/3-1, 	y2, 					count);
-				devide(paper, ((x1+1)+2*(x2+1))/3, 	y1, 					x2, 					(2*(y1+1)+(y2+1))/3-1, 	count);
-				devide(paper, ((x1+1)+2*(x2+1))/3, 	(2*(y1+1)+(y2+1))/3, 	x2, 					((y1+1)+2*(y2+1))/3-1, 	count);
-				devide(paper, ((x1+1)+2*(x2+1))/3, 	((y1+1)+2*(y2+1))/3, 	x2, 					y2, 					count);
-			}
+		else{
+			int x1 = (2*(startX+1)+(endX+1))/3;
+			int x2 = ((startX+1)+2*(endX+1))/3;
+			int y1 = (2*(startY+1)+(endY+1))/3;
+			int y2 = ((startY+1)+2*(endY+1))/3;
+			devide(paper, startX, 	startY, x1-1, y1-1, array);
+			devide(paper, startX, 	y1, 	x1-1, y2-1, array);
+			devide(paper, startX, 	y2, 	x1-1, endY, array);
+			devide(paper, x1, 		startY, x2-1, y1-1, array);
+			devide(paper, x1, 		y1,		x2-1, y2-1, array);
+			devide(paper, x1, 		y2, 	x2-1, endY, array);
+			devide(paper, x2, 		startY, endX, y1-1, array);
+			devide(paper, x2, 		y1, 	endX, y2-1, array);
+			devide(paper, x2, 		y2, 	endX, endY, array);
 		}
 	}
 
-	void getAnswer(int[] count){
+	static void print(int[] count){
 		System.out.println(count[0]);
 		System.out.println(count[1]);
 		System.out.println(count[2]);
-	}
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();		
-		int[][] paper = new int[n][n];
-		int x1 = 0;
-		int x2 = n-1;
-		int y1 = 0;
-		int y2 = n-1;
-		int[] count = new int[3];
-		count[0] = 0;	count[1] = 0;	count[2] = 0;
-
-		for(int i=0 ; i<n ; i++){
-			for(int j=0 ; j<n ; j++){
-				paper[i][j] = sc.nextInt();
-			}
-		}//입력 완료
-
-		new DividePaper(paper, x1, y1, x2, y2, count, n);
-
-		sc.close();
 	}
 }
