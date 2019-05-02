3f1c8bf60a9e220fa3900b3740cb5b835dc52dc1
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-05-02 21:53:57 +0900

    5/2

--- Algorithm/src/Sorting/MergeSort.java
+++ Algorithm/src/Sorting/MergeSort.java
@@ -1,78 +1,68 @@
-package Sorting;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class MergeSort {
+public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException{
-
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 
 		int n = Integer.parseInt(br.readLine());
+
 		int[] ar = new int[n];
 
 		for(int i=0 ; i<n ; i++){
 			ar[i] = Integer.parseInt(br.readLine());
 		}
-		
+
 		mergeSort(ar,0,ar.length-1);
 
-		for(int i=0 ; i<ar.length ; i++){
-			sb.append(ar[i]+"\n");
+		for(int i : ar){
+			sb.append(i).append("\n");
 		}
 		sb.deleteCharAt(sb.length()-1);
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-
+		System.out.print(sb.toString());
 	}
 
-	private static void mergeSort(int[] ar, int left, int right) {
-
-		int mid = (left+right)/2;
-
-		if(left!=right){
-			mergeSort(ar, left, mid);
-			mergeSort(ar, mid+1, right);
-			merge(ar, left, right);
+	static void mergeSort(int[] ar, int start, int end){
+		if(start<end){
+			int mid = (start+end)/2;
+			mergeSort(ar, start, mid);
+			mergeSort(ar, mid+1, end);
+			merge(ar,start,end);
 		}
-
 	}
 
-	private static void merge(int[] ar, int left, int right) {
+	static void merge(int[] ar, int start, int end){
+		int[] ar2 = new int[end-start+1];
 
-		int mid = (left+right)/2;		
-		int[] ar2 = new int[ar.length];
-		int l = left;
-		int m = mid+1;
-		int l2 = l;
+		int mid = (start+end)/2;
+		int mid2 = mid+1;
+		int a = start;
 
-		while(l<=mid && m<=right){
-			if(ar[l]>=ar[m]){
-				ar2[l2++] = ar[m++];
+		int index = 0;
+
+		while(a<=mid && mid2<=end){
+			if(ar[a]<=ar[mid2]){
+				ar2[index++] = ar[a++];
 			}
 			else{
-				ar2[l2++] = ar[l++];
+				ar2[index++] = ar[mid2++];
 			}
 		}
-		if(l>mid){
-			while(l2<=right){
-				ar2[l2++] = ar[m++];
-			}
-		}
-		else{
-			while(l<=mid){
-				ar2[l2++] = ar[l++]; 
-			}
+
+		while(a<=mid){
+			ar2[index++] = ar[a++];
 		}		
-		for(int i=left ; i<=right ; i++){
-			ar[i] = ar2[i];
+		while(mid2<=end){
+			ar2[index++] = ar[mid2++];
+		}
+
+		index = 0;
+
+		for(int i=start ; i<=end ; i++){
+			ar[i] = ar2[index++];
 		}
 	}
 }
