2ff990b6d808bb86d22a8f9f562426eba2496449
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-05-03 10:13:29 +0900

    5/3

--- Algorithm/src/Sorting/MergeSort.java
+++ Algorithm/src/Sorting/MergeSort.java
@@ -1,8 +1,10 @@
+package Sorting;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class MergeSort {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
