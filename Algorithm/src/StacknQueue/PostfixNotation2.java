82e3be04f088cda921f26ef3d3ba7b23c2605d68
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-05-12 14:25:57 +0900

    5/12

---
+++ Algorithm/src/StacknQueue/PostfixNotation2.java
@@ -0,0 +1,52 @@
+package StacknQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class PostfixNotation2 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		char[] input = br.readLine().toCharArray();
+
+		int[] ar = new int[n];
+
+		for(int i=0 ; i<n ; i++){
+			ar[i] = Integer.parseInt(br.readLine());
+		}
+
+		Stack<Double> st = new Stack<>();
+
+		for(int i=0 ; i<input.length ; i++){
+			char now = input[i];
+
+			if(now>='A' && now<='Z'){
+				st.push((double)ar[(int)now-65]);
+			}
+
+			else{
+				double b = st.pop();
+				double a = st.pop();
+				double temp = 0;
+				switch(now){
+				case '+':
+					temp = a+b; break;
+				case '-':
+					temp = a-b; break;
+				case '*':
+					temp = a*b; break;
+				case '/':
+					temp = a/b; break;
+				}
+				st.push(temp);
+			}
+		}
+		System.out.print(String.format("%.2f", st.pop()));
+	}
+}
