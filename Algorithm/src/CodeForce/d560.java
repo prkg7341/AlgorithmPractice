ba82238d7253697b00bb4e8b5b3c8dd98b017404
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2019-05-15 14:54:20 +0900

    5/15

---
+++ Algorithm/src/CodeForce/d560.java
@@ -0,0 +1,42 @@
+package CodeForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class d560 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		test:
+			for(int tt=0 ; tt<t ; tt++){
+				int n = Integer.parseInt(br.readLine());
+				int max = 0;
+				int min = 1234567;
+
+				String[] input = br.readLine().split(" ");
+
+				for(int i=0 ; i<n ; i++){
+					int now = Integer.parseInt(input[i]);
+					max = Math.max(max, now);
+					min = Math.min(min, now);
+				}
+
+				long num = max*min;
+
+				for(int i=0 ; i<n ; i++){
+					int now = Integer.parseInt(input[i]);
+					if(num%now != 0){
+						System.out.print(-1);
+						continue test;
+					}
+				}
+
+				System.out.println(num);
+			}
+	}
+}
