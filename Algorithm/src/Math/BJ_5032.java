8e547dd04bdd22929bb0341825694a37e57770df
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2020-01-06 00:23:42 +0900

    1/5 수정 (카운트X)

--- Algorithm/src/Math/BJ_5032.java
+++ Algorithm/src/Math/BJ_5032.java
@@ -1,5 +1,33 @@
 package Math;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class BJ_5032 {
 
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] input = br.readLine().split(" ");
+
+		int now = Integer.parseInt(input[0]);
+		int add = now + Integer.parseInt(input[1]);
+		int price = Integer.parseInt(input[2]);
+
+		int result = 0;
+
+		while(add>=price) {
+			result += add/price;
+			add = add/price + add%price;
+		}
+
+		bw.write(String.valueOf(result));
+
+		bw.flush();
+	}
 }
