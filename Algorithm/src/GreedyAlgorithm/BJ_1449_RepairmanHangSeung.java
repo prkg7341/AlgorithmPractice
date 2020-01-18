4814f921bd0d533af6da309ccee5c34160fb7140
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2020-01-19 00:51:20 +0900

    1/18

--- Algorithm/src/GreedyAlgorithm/BJ_1449_RepairmanHangSeung.java
+++ Algorithm/src/GreedyAlgorithm/BJ_1449_RepairmanHangSeung.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class RepairmanHangSeung {
+public class BJ_1449_RepairmanHangSeung {
 
 	public static void main(String[] args) throws IOException {
 
