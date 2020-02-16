fb403b2d6105ce2168c70e8dad6f3af426dc6771
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2020-02-16 17:13:00 +0900

    2/16

--- Algorithm/src/List/BJ_1158.java
+++ Algorithm/src/List/BJ_1158.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class JosephusProblem {
+public class BJ_1158 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
--- Algorithm/src/List/BJ_2346.java
+++ Algorithm/src/List/BJ_2346.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class BreakingBalloon {
+public class BJ_2346 {
 
 	public static void main(String[] args) throws IOException {
 
