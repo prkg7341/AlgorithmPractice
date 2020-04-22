b6724436f39881798d926a04893c0363b20cd643
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2020-04-22 14:51:22 +0900

    4/22

--- Algorithm/src/BitMask/BJ_11723.java
+++ Algorithm/src/BitMask/BJ_11723.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Set {
+public class BJ_11723 {
 
 	public static void main(String[] args) throws IOException {	
 
--- Algorithm/src/BitMask/BJ_2098.java
+++ Algorithm/src/BitMask/BJ_2098.java
@@ -3,13 +3,13 @@ package BitMask;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class TSP_nonefficient {
+public class BJ_2098 {
 
 	static int n;
 	static int min = Integer.MAX_VALUE;
-	static int[][] ar;
-	static int[][] dp;
-	static int MAX_VAL = 100000000;
+	static int[][] ar; // 입력값 저장
+	static int[][] dp; // 각 상황별(a 마을에 마지막으로 방문했고, b 마을들에 방문한 상황일때) 최소값 저장
+	final static int IMPOSSIBLE = 100000000; // 길없음
 
 	public static void main(String[] args) throws IOException {	
 
@@ -20,11 +20,12 @@ public class TSP_nonefficient {
 		ar = new int[n][n];
 		dp = new int[n][1<<n];
 
+		// 입력부
 		for(int i=0 ; i<n ; i++){
 			for(int j=0 ; j<n ; j++){
 				int now = sc.nextInt();
 				if(i!=j && now==0){
-					ar[i][j] = MAX_VAL;
+					ar[i][j] = IMPOSSIBLE;
 				}
 				else
 					ar[i][j] = now;
@@ -38,20 +39,26 @@ public class TSP_nonefficient {
 		System.out.println(min);
 	}
 
-	private static void tsp(int now, int bit, int sum) {
-		if(bit==(1<<n)-1){
+	private static void tsp(int now, int visited, int sum) {
+		// 후보값 중 최소값 저장
+		if(visited==(1<<n)-1){
 			min = Math.min(min, sum+ar[now][0]);
 		}
 
 		else{
 			for(int i=0 ; i<n ; i++){
-				if(i!=now && (bit & 1<<i)==0){
-					int added = ar[now][i];
-					if(added >= MAX_VAL){
+				// 지금 마을이 아니고, 아직 방문하지 않은 마을일 때
+				if(i!=now && (visited & 1<<i)==0){
+					int added = ar[now][i]; // 거리
+					// 갈수없으면 제외
+					if(added >= IMPOSSIBLE){
 						continue;
 					}
-					int newbit = bit|1<<i;
+					// 방문 마을 업데이트
+					int newbit = visited|1<<i;
+					// 거리 합계
 					int newsum = sum+added;
+					// 동적 계획법 적용 부분 (마지막 마을 기준 같은 마을들 방문했을때 dp배열에 최솟값 저장)
 					if(dp[i][newbit]!=0) {
 						if(dp[i][newbit] > newsum){
 							dp[i][newbit] = newsum;
