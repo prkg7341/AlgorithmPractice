b21b115f6b4bc2ef8ae0244672f49aef1194db49
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2020-05-06 18:36:37 +0900

    5/6

--- Algorithm/src/PrefixSum/BJ_11441.java
+++ Algorithm/src/PrefixSum/BJ_11441.java
@@ -1,4 +1,4 @@
-package DynamicProgramming;
+package PrefixSum;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,18 +6,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BJ_11659 {
+public class BJ_11441 {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		int n = Integer.parseInt(br.readLine());
+
 		String[] input = br.readLine().split(" ");
-
-		int num = Integer.parseInt(input[1]);
-
-		input = br.readLine().split(" ");
 
 		int[] ar = new int[input.length+1];
 
@@ -25,7 +23,9 @@ public class BJ_11659 {
 			ar[i] = ar[i-1] + Integer.parseInt(input[i-1]);
 		}
 
-		for(int i=0 ; i<num ; i++) {
+		n = Integer.parseInt(br.readLine());
+
+		for(int i=0 ; i<n ; i++) {
 			input = br.readLine().split(" ");
 			int from = Integer.parseInt(input[0]);
 			int to = Integer.parseInt(input[1]);
