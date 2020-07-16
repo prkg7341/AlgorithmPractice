f7f3393470928e5add30c4b778252cae5e104174
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2020-07-16 22:03:12 +0900

    7/16

--- Algorithm/src/SegmentTree/BJ_2357.java
+++ Algorithm/src/SegmentTree/BJ_2357.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class MinMaxValue {
+public class BJ_2357 {
 
 	public static void main(String[] args) throws IOException {
 
