1a1102781ae19ec0310bd091f849a66584f95045
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2020-09-11 05:27:10 +0900

    9/11

--- Algorithm/src/Kakao2020/K4.java
+++ Algorithm/src/Kakao2020/K4.java
@@ -1,35 +1,66 @@
 package Kakao2020;
 
+import java.util.HashMap;
+
 public class K4 {
 	public int[] solution(String[] words, String[] queries) {
 		int[] answer = new int[queries.length];
 
+		Node[] roots = new Node[10001];
+		Node[] roots_ = new Node[10001];
+
+		for(int i=1 ; i<roots.length ; i++) {
+			roots[i] = new Node();
+			roots_[i] = new Node();
+		}
+
+		for(int i=0 ; i<words.length ; i++){
+			String now = words[i];
+			insert(roots[now.length()], now, 0);
+			now = new StringBuilder(now).reverse().toString();
+			insert(roots_[now.length()], now, 0);
+		}
+
 		for(int i=0 ; i<queries.length ; i++) {
 			String now = queries[i];
-
-			int count = 0;
-
-			for(int k=0 ; k<words.length ; k++) {
-				String comp = words[k];
-				if(!same(now, comp)) continue;
-				count++;
-//				System.out.println("count");
+			if(now.charAt(0)=='?') {
+				if(now.charAt(now.length()-1)=='?') {
+					for(Character c : roots_[now.length()].children.keySet()) {
+						answer[i] += roots_[now.length()].children.get(c).count;
+					}
+					continue;
+				}
+				now = new StringBuilder(now).reverse().toString();
+				answer[i] = dfs(roots_[now.length()], now, 0);
 			}
-
-			answer[i] = count;
+			else {
+				answer[i] = dfs(roots[now.length()], now, 0);
+			}
 		}
 
 		return answer;
 	}
 
-	private boolean same(String now, String comp) {
-		if(now.length()!=comp.length()) return false;
-		for(int i=0 ; i<now.length() ; i++) {
-			if(now.charAt(i)!='?') {
-				if(now.charAt(i)!=comp.charAt(i)) return false;
-			}
+	private int dfs(Node node, String word, int index) {
+		if(word.charAt(index)=='?') return node.count;
+		if(!node.children.containsKey(word.charAt(index))) {
+			return 0;
 		}
-//		System.out.println(now+" "+comp);
-		return true;
+		return dfs(node.children.get(word.charAt(index)), word, index+1);
+	}
+
+	private void insert(Node node, String word, int index) {
+		if(index==word.length()) return;
+		char now = word.charAt(index);
+		if(!node.children.containsKey(now)) {
+			node.children.put(now, new Node());
+		}
+		else node.children.get(now).count++;
+		insert(node.children.get(now), word, index+1);
+	}
+
+	static class Node{
+		HashMap<Character, Node> children = new HashMap<>();
+		int count = 1;
 	}
 }
