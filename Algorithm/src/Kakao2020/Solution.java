0eca3cb7e9bbf193a17ebbe46b0c14feecaf0d87
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2020-09-11 23:42:46 +0900

    9/11

--- Algorithm/src/Kakao2020/Solution.java
+++ Algorithm/src/Kakao2020/Solution.java
@@ -18,13 +18,18 @@ public class Solution {
 //		System.out.println(s.solution(")("));
 //		System.out.println(s.solution("()))((()"));
 		
-		K3 s = new K3();
-		System.out.println(s.solution(new int[][]{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}, 
-				new int[][] {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}));
+//		K3 s = new K3();
+//		System.out.println(s.solution(new int[][]{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}, 
+//				new int[][] {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}));
 		
-		
+
 //		K4 s = new K4();
 //		System.out.println(Arrays.toString(s.solution(new String[]{"frodo", "front", "frost", "frozen", "frame", "kakao"}, 
-//				new String[]{"fro??", "????o", "fr???", "fro???", "pro?"})));
+//				new String[]{"fro??", "????o", "fr???", "fro???", "pro?", "?????"})));
+
+		K5 s = new K5();
+		System.out.println(Arrays.deepToString(s.solution(5, new int[][]{{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}})));
+		System.out.println(Arrays.deepToString(s.solution(5, new int[][]{{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{4,1,1,1},{4,1,0,1},{3,2,1,1}})));
+		System.out.println(Arrays.deepToString(s.solution(5, new int[][]{{0,0,0,1},{2,0,0,1},{4,0,0,1},{0,1,1,1},{1,1,1,1},{2,1,1,1},{3,1,1,1},{2,0,0,0},{1,1,1,0},{2,2,0,1}})));		
 	}
 }
