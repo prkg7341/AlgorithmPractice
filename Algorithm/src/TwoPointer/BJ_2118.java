0a4a612dfe417e4b9803b9c56d44eedeaf2a20b5
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2021-03-17 19:21:42 +0900

    21.3.17

--- Algorithm/src/TwoPointer/BJ_2118.java
+++ Algorithm/src/TwoPointer/BJ_2118.java
@@ -1,4 +1,4 @@
-package Pointer;
+package TwoPointer;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,42 +6,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BJ_2003 {
+public class BJ_2118 {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int num = Integer.parseInt(input[1]);
+		int n = Integer.parseInt(br.readLine());
 
-		int[] ar = new int[n+1];
-
-		input = br.readLine().split(" ");
+		int[] ar = new int[2*n+1];
 
 		for(int i=1 ; i<=n ; i++) {
-			int now = Integer.parseInt(input[i-1]);
+			int now = Integer.parseInt(br.readLine());
 			ar[i] = ar[i-1] + now;
+			ar[i+n] = now;
+		}
+
+		for(int i=n+1 ; i<ar.length ; i++) {
+			ar[i] += ar[i-1];
 		}
 
 		int a = 0;
 		int b = 1;
-		int count = 0;
+		int max = 0;
 
-		while(b<=n) {			
-			int now = ar[b] - ar[a];
+		while(max!=ar[n]/2 && a<=n) {			
+			int now = ar[b]-ar[a];
 
-			if(now==num) {				
-				count++;
-				a++; b++;
+			max = Math.max(max, Math.min(now, ar[n]-now));
+
+			if(now*2>ar[n]) {
+				a++;
 			}
-			else if(now>num) a++;			
-			else b++;
+			else {
+				b++;
+			}
 		}
 
-		bw.write(Integer.toString(count));
+		bw.write(Integer.toString(max));
 
 		bw.flush();
 	}
