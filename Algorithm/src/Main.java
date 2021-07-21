efdb89ed86c661fc08742e2439616069666c159a
Jaewoo Oh <40709295+prkg7341@users.noreply.github.com>
2021-07-21 09:52:03 +0900

    210721

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -4,20 +4,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
-
+public class Main{
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		int n = Integer.parseInt(br.readLine());
-		String[] input = br.readLine().split(" ");
-
+		
 		for(int i=0 ; i<n ; i++) {
-			
+			System.out.println();
 		}
-
+		
 		bw.flush();
 	}
 }
