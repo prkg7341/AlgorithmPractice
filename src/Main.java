2203bd31329ccf179e7714ed05dd6b36263388a0
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2021-08-12 09:19:27 +0900

    Main 수정 및 깃 테스트

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		for(int i=0 ; i<n ; i++) {
-			System.out.println();
+		int n = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < args.length; i++) {
+			
 		}
-		
-		bw.flush();
 	}
 }
